689f3dada5a0f943804c93cab8839f9e5b7798aa
Geordon Worley <vadixidav@gmail.com>
2017-03-28 11:35:31 -0400

    separate TotalMemory into its own module "mem"

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,9 @@ extern crate rand;
 extern crate heapsize;
 
 mod vec;
+mod mem;
 
-use heapsize::HeapSizeOf;
+use vec::*;
+use mem::TotalMemory;
 
 use std::convert::TryFrom;
-use std::mem;
-
-pub trait TotalMemory {
-    fn total_memory(&self) -> usize;
-}
-
-impl<T> TotalMemory for T where T: HeapSizeOf {
-    fn total_memory(&self) -> usize {
-        mem::size_of::<Self>() + self.heap_size_of_children()
-    }
-}
