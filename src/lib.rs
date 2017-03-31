3ce7fd3088ed3e34a7e960f198e4a63a7fa68b5b
Geordon Worley <vadixidav@gmail.com>
2017-03-31 12:54:17 -0400

    add new tracked double ended queue vector

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,12 @@ use mem::TotalMemory;
 
 use std::convert::TryFrom;
 
+/// A Gapush `Program` is a state machine which tracks the memory consumption of an arbitrary program and executes it
+/// without going over a specified limit amount of memory (`max_memory`).
 pub struct Program<Instruction> {
-    /// Instructions are popped from this stack to be executed.
+    /// The limit of how much memory is allowed to be used.
+    pub max_memory: usize,
+    /// Instructions are popped from this stack to be executed and this stack is not directly accessed.
     pub execute_stack: TrackedVec<Instruction>,
     /// This is a stack for handling instructions as data.
     pub instruction_stack: TrackedVec<Instruction>,
--- src/vec.rs
+++ src/vec.rs
@@ -1,6 +1,7 @@
 use TotalMemory;
 
-use std::vec::IntoIter;
+use std::vec;
+use std::collections::vec_deque::{self, VecDeque};
 use std::mem;
 
 pub struct TrackedVec<T> {
@@ -35,6 +36,11 @@ impl<T> TrackedVec<T>
     }
 
     #[inline]
+    pub fn get(&mut self, ix: usize) -> Option<&T> {
+        self.vec.get(ix)
+    }
+
+    #[inline]
     pub fn into_iter(self) -> TrackedIter<T> {
         TrackedIter {
             iter: self.vec.into_iter(),
@@ -50,7 +56,7 @@ impl<T> TotalMemory for TrackedVec<T> {
 }
 
 pub struct TrackedIter<T> {
-    iter: IntoIter<T>,
+    iter: vec::IntoIter<T>,
     size: usize,
 }
 
@@ -62,6 +68,75 @@ impl<T> Iterator for TrackedIter<T> {
 }
 
 impl<T> TotalMemory for TrackedIter<T> {
+    fn total_memory(&self) -> usize {
+        self.size
+    }
+}
+
+pub struct TrackedDeque<T> {
+    vec: VecDeque<T>,
+    size: usize,
+}
+
+impl<T> TrackedDeque<T>
+    where T: TotalMemory
+{
+    #[inline]
+    pub fn new() -> TrackedDeque<T> {
+        TrackedDeque {
+            vec: VecDeque::new(),
+            size: mem::size_of::<Self>(),
+        }
+    }
+
+    #[inline]
+    pub fn push(&mut self, e: T) {
+        self.size += e.total_memory();
+        self.vec.push_back(e);
+    }
+
+    #[inline]
+    pub fn pop(&mut self) -> Option<T> {
+        let r = self.vec.pop_front();
+        if let Some(ref r) = r {
+            self.size -= r.total_memory();
+        }
+        r
+    }
+
+    #[inline]
+    pub fn get(&mut self, ix: usize) -> Option<&T> {
+        self.vec.get(ix)
+    }
+
+    #[inline]
+    pub fn into_iter(self) -> TrackedDeqIter<T> {
+        TrackedDeqIter {
+            iter: self.vec.into_iter(),
+            size: self.size,
+        }
+    }
+}
+
+impl<T> TotalMemory for TrackedDeque<T> {
+    fn total_memory(&self) -> usize {
+        self.size
+    }
+}
+
+pub struct TrackedDeqIter<T> {
+    iter: vec_deque::IntoIter<T>,
+    size: usize,
+}
+
+impl<T> Iterator for TrackedDeqIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.iter.next()
+    }
+}
+
+impl<T> TotalMemory for TrackedDeqIter<T> {
     fn total_memory(&self) -> usize {
         self.size
     }
